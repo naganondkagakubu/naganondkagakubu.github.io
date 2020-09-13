64f4dcb3613cb6f3a28a767a30995a577561ba6e
myaya7 <you200315@outlook.jp>
2020-09-13 22:36:45 +0900

    9/1313/

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -1,9 +1,11 @@
+//メニューのON/OFF
 jQuery(function(){
     jQuery(".menu-btn").on("click",function(){
         jQuery(".menu-btn, .menu").toggleClass("is-active");
     });
 }());
 
+//領域外クリックでメニューを閉じる
 jQuery(function(){
     jQuery(".whole").on("click", function(event){
         if (!jQuery(event.target).closest(".menu-btn").length){
