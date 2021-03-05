76eb00528c9726b50cf90ced8574f40fa4537725
naganonkagakubu <kagakubu_isgod@outlook.com>
2021-03-05 17:16:43 +0900

    haikei hennkou

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -30,8 +30,8 @@ function PW(){
     PW1=prompt("パスワードを入力してください","")
     if(PW1=="URL")
         location.href="https://naganonkagakubu.github.io/HAL_TS/index.html";
-        
+
     else if(PW1=="HAL")
         location.href="https://naganonkagakubu.github.io/HAL_TS/TS14789632147898452.html";
-    
+
 }
