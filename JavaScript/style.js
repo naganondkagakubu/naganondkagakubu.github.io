1b431323c8943fe96ed64d5d2878676fba8a2f84
myaya7 <you200315@outlook.jp>
2021-06-13 00:06:52 +0900

    tramp

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -27,15 +27,15 @@ function TP_URL(){
 
 //PW機能
 function PW(){
-    PW1=prompt("パスワードを入力してください","")
+    PW1=prompt("URLを入力してください","")
     if(PW1=="URL")
         location.href="https://naganonkagakubu.github.io/HAL_TS/index.html";
-
     else if(PW1=="HAL")
         location.href="https://naganonkagakubu.github.io/HAL_TS/TS14789632147898452.html";
+    else if(PW1=="PLAY")
+        location.href="https://naganonkagakubu.github.io/PLAY/index.html";
     else if(PW1=="TEST")
         location.href="https://naganonkagakubu.github.io/testtest/index.html";
     else
         console.log("パスワードが違うﾖ")
-
 }
---
+++ PLAY/JS/card.js
@@ -0,0 +1,40 @@
+let numin = document.getElementById("numin")
+let set_buttun = document.getElementById("set")
+let start_buttun = document.getElementById("start")
+let numout = document.getElementById("numout")
+let reset_buttun = document.getElementById("reset")
+let back_buttun = document.getElementById("back")
+let numback = document.getElementById("numback")
+
+let n = 0
+let m = 0
+let ramdoms = []
+
+function Ramdom(M){
+    if(ramdoms.length < M){
+        while(true){
+            n  = Math.floor(1 + Math.random() * M);
+            if(!ramdoms.includes(n)){
+                ramdoms.push(n);
+                break;
+            }
+        }
+        return n;
+    }
+    else{
+        return "全部引きました";
+    }
+}
+
+function Reset(n){
+    let idx = ramdoms.indexOf(n);
+    if(idx >= 0){
+        ramdoms.splice(idx, 1);
+    }
+}
+
+set_buttun.onclick = function(){m = numin.value};
+start_buttun.onclick = function(){numout.value = Ramdom(m)};
+reset_buttun.onclick = function(){ramdoms.length = 0;};
+back_buttun.onclick = function(){Reset(parseInt(numback.value))};
+
