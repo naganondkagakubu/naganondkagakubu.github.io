65a9aca4b688849da2d54a6db6726514bfef643f
myaya7 <you200315@outlook.jp>
2021-06-13 18:20:32 +0900

    fdsf

--- JavaScript/passward.js
+++ JavaScript/passward.js
@@ -1,5 +1,5 @@
 function PW(){
-    PW1=prompt("URLを入力してください","")
+    PW1=prompt("パスワードを入力してください","")
     if(PW1=="URL")
         location.href="https://naganonkagakubu.github.io/HAL_TS/index.html";
     else if(PW1=="HAL")
--- JavaScript/style.js
+++ JavaScript/style.js
@@ -27,7 +27,7 @@ function TP_URL(){
 
 //PW機能
 function PW(){
-    PW1=prompt("URLを入力してください","")
+    PW1=prompt("PWを入力してください","")
     if(PW1=="URL")
         location.href="https://naganonkagakubu.github.io/HAL_TS/index.html";
     else if(PW1=="HAL")
