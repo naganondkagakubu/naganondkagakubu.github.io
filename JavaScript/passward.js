4bb2ddf01c4971b14dbcdb9d91b8410fc1d3041f
myaya7 <you200315@outlook.jp>
2021-06-13 18:34:27 +0900

    tp

--- JavaScript/passward.js
+++ JavaScript/passward.js
@@ -1,6 +1,6 @@
 function PW(){
-    PW1=prompt("パスワードを入力してください","")
-    if(PW1=="URL")
+    PW1=prompt("URLを入力してください","")
+    if(PW1=="TP")
         location.href="https://naganonkagakubu.github.io/HAL_TS/index.html";
     else if(PW1=="HAL")
         location.href="https://naganonkagakubu.github.io/HAL_TS/TS14789632147898452.html";
@@ -9,5 +9,5 @@ function PW(){
     else if(PW1=="TEST")
         location.href="https://naganonkagakubu.github.io/testtest/index.html";
     else
-        console.log("パスワードが違うﾖ")
+        alert("まだ対応してないﾖ")
 }
