9581b838c95f757cebfae10410548ef04b226558
myaya7 <you200315@outlook.jp>
2021-06-13 18:37:23 +0900

    tpp

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -27,8 +27,8 @@ function TP_URL(){
 
 //PW機能
 function PW(){
-    PW1=prompt("PWを入力してください","")
-    if(PW1=="URL")
+    PW1=prompt("URLを入力してください","")
+    if(PW1=="TP")
         location.href="https://naganonkagakubu.github.io/HAL_TS/index.html";
     else if(PW1=="HAL")
         location.href="https://naganonkagakubu.github.io/HAL_TS/TS14789632147898452.html";
@@ -37,5 +37,5 @@ function PW(){
     else if(PW1=="TEST")
         location.href="https://naganonkagakubu.github.io/testtest/index.html";
     else
-        console.log("パスワードが違うﾖ")
+        alert("パスワードが違うﾖ")
 }
