ee78c9fa1c5d7f4e4bfa7a6ebe111bc1be8a594f
myaya7 <you200315@outlook.jp>
2021-06-20 01:02:57 +0900

    chess

--- JavaScript/PLAY/syougi.js
+++ JavaScript/PLAY/syougi.js
@@ -1,4 +1,12 @@
-//+-で味方と敵、10or00(10の位)で表裏
+//+-で味方と敵、10or00(10の位)で表裏 (8は味方香、-8は敵香、18は味方成香)
+
+//変数宣言
+for (let i = 1; i<=9; i++){
+    for(let j=1; j<=9; j++){
+        let ij = (10 * i) + j;
+        eval("var img_" + ij + "= document.getElementById('img" + ij + "');")
+    }
+}
 
 let S_get = []; //先手が取った駒
 let G_get = []; //後手が取った駒
